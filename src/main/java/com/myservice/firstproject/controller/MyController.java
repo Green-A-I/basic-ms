be5e6e6375092ed109880bbcbe4201db50af403d
HKC <HKC@Swapnil-Personal>
2022-12-23 15:24:57 +0530

    service layer created

--- src/main/java/com/myservice/firstproject/controller/MyController.java
+++ src/main/java/com/myservice/firstproject/controller/MyController.java
@@ -3,6 +3,8 @@ package com.myservice.firstproject.controller;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class MyController {
 	@GetMapping("/getCustomerName")
 	public String getName() {
 		return "Hi My name is Swapnil baba";
+	}
+	
+	@PostMapping("/calculateLength")
+	public String calculateLength(@RequestBody String name) {
+		return "hello "+name;
 	}
 	
 	
